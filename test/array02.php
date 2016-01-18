a54c377af300316741c8834bfae727dd1b6525c2
Jiangyoung <jiangyoungzh@163.com>
2016-01-18 19:54:41 +0800

    c12

--- test/array02.php
+++ test/array02.php
@@ -1,28 +1,15 @@
 <?php
+
 $t1 = microtime(true);
 $arr = range(1,10000);
 $t2 = microtime(true);
-echo 'create arr : '.($t2-$t1).PHP_EOL;
+//echo 'create arr : '.($t2-$t1).PHP_EOL;
+
+
+
+$t1 = microtime(true);
 $t1 = microtime(true);
 $m1 = memory_get_usage();
-$arr1 = array_map(function($val){return $val*3+2;},$arr);
-$t2 = microtime(true);
-echo 'closer function : '.($t2-$t1).PHP_EOL;
-$m2 = memory_get_usage();
-echo 'memory used : '.(($m2-$m1)/1024).PHP_EOL;
-
-
-$t1 = microtime(true);
-$t1 = microtime(true);
-$arr2 = array_map('f',$arr);
-$t2 = microtime(true);
-echo 'function f: '.($t2-$t1).PHP_EOL;
-$m2 = memory_get_usage();
-echo 'memory used : '.(($m2-$m1)/1024).PHP_EOL;
-
-
-$t1 = microtime(true);
-$t1 = microtime(true);
 $arr3 = array();
 foreach($arr as $v){
 	$arr3[] = $v*3+2;
@@ -33,6 +20,29 @@ $m2 = memory_get_usage();
 echo 'memory used : '.(($m2-$m1)/1024).PHP_EOL;
 
 
-function f($x){
+$t1 = microtime(true);
+$m1 = memory_get_usage();
+$arr1 = array_map(function($val){return $val*3+2;},$arr);
+$t2 = microtime(true);
+echo 'closure function : '.($t2-$t1).PHP_EOL;
+$m2 = memory_get_usage();
+echo 'memory used : '.(($m2-$m1)/1024).PHP_EOL;
+$f = function($x){
 	return $x*3+2;
+};
+
+$t1 = microtime(true);
+$t1 = microtime(true);
+$m1 = memory_get_usage();
+$arr2 = array_map($f,$arr);
+$t2 = microtime(true);
+echo 'function f: '.($t2-$t1).PHP_EOL;
+$m2 = memory_get_usage();
+echo 'memory used : '.(($m2-$m1)/1024).PHP_EOL;
+
+$x = f2();
+var_dump($x);
+function f2(){
+	//$arr = array();
+	return $val == 1 ? $a : $b ;
 }
--- test/array03.php
+++ test/array03.php
@@ -1,12 +1,55 @@
 <?php
 $m1 = memory_get_usage();
-echo 'memory used : '.($m1/1024).PHP_EOL;
 $t1 = microtime(true);
 $arr = array();
-for($i=0;$i<=3000;$i++){
-	$arr[] = array('id'=>$i+1,'name'=>'sdkjflsjf');
+for($i=0;$i<=30000;$i++){
+	$arr[] = array('id'=>$i+1,'name'=>'sdkjflsjf','type'=>3);
 }
 $t2 = microtime(true);
-echo 'create arr : '.($t2-$t1).PHP_EOL;
+//echo 'create arr : '.($t2-$t1).PHP_EOL;
+$m2 = memory_get_usage();
+//echo 'memory used : '.(($m2-$m1)/1024).PHP_EOL;
+
+$t1 = microtime(true);
+$m1 = memory_get_usage();
+$arr1 = array_column($arr,'id');
+
+$t2 = microtime(true);
+echo 'array_column : '.($t2-$t1).PHP_EOL;
+$m2 = memory_get_usage();
+echo 'memory used : '.(($m2-$m1)/1024).PHP_EOL;
+
+
+$t1 = microtime(true);
+$t1 = microtime(true);
+$m1 = memory_get_usage();
+$arr2 = array();
+foreach($arr as $v){
+	$arr2[$v['id']] = $v;
+}
+$t2 = microtime(true);
+echo 'foreach : '.($t2-$t1).PHP_EOL;
+$m2 = memory_get_usage();
+echo 'memory used : '.(($m2-$m1)/1024).PHP_EOL;
+
+
+$t1 = microtime(true);
+$m1 = memory_get_usage();
+$arr3 = array_column($arr,'id','type');
+$t2 = microtime(true);
+echo 'array_column : '.($t2-$t1).PHP_EOL;
+$m2 = memory_get_usage();
+echo 'memory used : '.(($m2-$m1)/1024).PHP_EOL;
+
+
+$t1 = microtime(true);
+$t1 = microtime(true);
+$m1 = memory_get_usage();
+$arr4 = array();
+foreach($arr as $v){
+	$arr4[$v['id']] = $v['type'];
+}
+$t2 = microtime(true);
+echo 'foreach : '.($t2-$t1).PHP_EOL;
 $m2 = memory_get_usage();
 echo 'memory used : '.(($m2-$m1)/1024).PHP_EOL;
