f0c06a4abb55fdc0634805562acdcd8f22c4401d
jiangyoung <jiangyoungzh@163.com>
2016-07-18 20:32:28 +0800

    ajax test

---
+++ js/ajax/server.php
@@ -0,0 +1,19 @@
+<?php
+$str = <<<EEE
+<!DOCTYPE html>
+<html>
+<head>
+<meta Content-Type="text/html;charset=utf-8" >
+</head>
+<body>
+<div id="container">
+	<div class="nav">
+	</div>
+	<div class="">
+	</div>
+</div>
+</body>
+</html>
+EEE;
+
+echo $str;
