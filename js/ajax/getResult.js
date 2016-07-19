cf6f7cd550fa9d046e26351256aee369922c7d96
jiangyoung <jiangyoungzh@163.com>
2016-07-19 18:15:03 +0800

    分页html合并js

--- js/ajax/getResult.js
+++ js/ajax/getResult.js
@@ -1,13 +1,13 @@
 /**
  * @brief 合并所有分页中的内容统一展示
  *
- * 1、修改参数或者代码。。
+ * 1、修改参数或者代码。。 （①开始页②结束页(有的页面可能是start和offse具体按不用页面修改)③网址④取具体内容的js代码）
  * 2、在需要的页面打开控制台
  * 3、复制改好的代码然后回车。
  */
-var start = 1;
-var end = 5;
-var baseUrl = "http://qiancheng.me/page/";  //随便找了个分页的测试网站。。。 
+var start = 1; //①
+var end = 5;   //②
+var baseUrl = "http://qiancheng.me/page/";  //③     (随便找了个分页的测试网站。。)
 //var result = [""];
 
 //execute
@@ -29,7 +29,8 @@ function getResult(url,start,end,res){
 			
 			//转document
 			var doc = (new DOMParser()).parseFromString(xmlhttp.responseText,"text/html");
-			console.log(doc.getElementsByClassName("container")[0].getElementsByClassName("home post-list")[0]);
+			//console.log(doc.getElementsByClassName("container")[0].getElementsByClassName("home post-list")[0]);
+			//④    (这里的内容按照页面具体格式自己研究。。 目的是取出实际内容)
 			res[start++] = doc.getElementsByClassName("container")[0].getElementsByClassName("home post-list")[0].innerHTML;			
 			getResult(url,start,end,res);
 		}else {
